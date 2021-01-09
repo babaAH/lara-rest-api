25fba45557b0ee2b77a871726190969d46d9007d
babaAH <shama_chich@icloud.com>
2021-01-09 15:24:11 +0300

    done

--- app/Http/Controllers/Room.php
+++ app/Http/Controllers/Room.php
@@ -12,6 +12,11 @@ use \App\Models\Booking as BookingModel;
 
 class Room extends Controller
 {
+    private $resp = [
+        "error" => false
+    ];
+    private $status = 200;
+
     /**
      * Display a listing of the resource.
      *
@@ -27,9 +32,10 @@ class Room extends Controller
             ->paginate(12)
             ->toArray();
 
-        return response()->json(
-            $rooms
-        );
+        return response()->json([
+            $this->resp,
+            "rooms" => $rooms
+        ]);
     }
 
     /**
@@ -44,17 +50,23 @@ class Room extends Controller
         
         $val = $this->makeStoreValidation($data);
 
-        $room = RoomModel::create([
-            'active'      => $data['active'],
-            'price'       => $data['price'],
-            'description' => $data['description'],
-        ]);
+        if($val->passes()){
+            $room = RoomModel::create([
+                'active'      => $data['active'],
+                'price'       => $data['price'],
+                'description' => $data['description'],
+            ]);
+
+            $this->resp["room_id"] = $room->id;
+        }else{
+            $this->resp["error"] = true;
+            $this->resp["msg"]   = $val->messages();
+            $this->status        = 400;
+        }
 
 
-        return response()->json([
-            'error' => false,
-            'roomId' => $room->id, 
-        ]);
+
+        return response()->json($this->resp, $this->status);
     }
 
     /**
@@ -65,59 +77,54 @@ class Room extends Controller
      */
     public function destroy($id)
     {
-        $room = RoomModel::find($id);
-
-        $this->deleteCoupliedBookings($room->bookings() ?? []);
-
-        $room->active = false;
-        $room->save();
-
-        return response()->json([
-            "error" => false,
-        ]);
+        $room = RoomModel::findOrFail($id);
+        $room->delete();
+        return response()->json($this->resp, $this->status);
     }
-        
+    
     /**
-     * deleteCoupliedBookings
+     * get column by ordering
      *
-     * @param  mixed $bookingsArr
-     * @return void
+     * @return string
      */
-    private function deleteCoupliedBookings($bookingsArr)
-    {
-        foreach($bookingsArr as $booking){
-            $booking->delete();
-            $booking->save();
-        }
-    }
-
     private function getOrderColumn()
     {
-        $val = \Validator::make([
-            'order' => \Request::get("order"), 
-        ],
-        [
-            'order' => 'in:price,created_at',
-        ]);
+        $val = \Validator::make(['order' => \Request::get("order"),],
+            [
+                'order' => 'in:price,created_at',
+            ]
+        );
 
         $val->passes() ? $columnName = \Request::get("order") : $columnName = 'created_at';
 
         return $columnName;
     }
-
+    
+    /**
+     * get Direction Value for sorting
+     *
+     * @return string
+     */
     private function getDirectionValue()
     {
-        
-        $val = \Validator::make([
-            'direction' => \Request::get("direction")
-        ],
-        [
-            'direction' => 'in:asc,desc'
-        ]);
+        $val = \Validator::make(['direction' => \Request::get("direction")],
+            [
+                'direction' => 'in:asc,desc'
+            ]
+        );
 
         $val->passes() ? $directionValue = \Request::get("direction") : $directionValue = 'asc';
         
         return $directionValue;
 
+    }
+
+    private function makeStoreValidation(Array $data)
+    {
+        return \Validator::make($data, [
+            'price'       => 'required|integer|between:0, 18446744073709551615', // 18446744073709551615  - max UNSIGNED INT in MySQL
+            'description' => 'required|string|min:25|max:8191',
+            'active'      => 'required|boolean',
+        ]);
     }
 }
